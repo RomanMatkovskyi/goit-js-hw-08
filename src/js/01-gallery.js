b03ee26c92043fb1543269c7946307363ddf08d6
RomanMatkovskyi <121770356+RomanMatkovskyi@users.noreply.github.com>
2023-05-31 23:35:20 +0200

    set parshal

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+let ulEl = document.querySelector('.gallery');
+let markup = galleryItems.map(item => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href=${item.original}>
+      <img class="gallery__image" src=${item.preview} alt='${item.description}' />
+   </a>
+</li>`;
+});
+ulEl.insertAdjacentHTML('beforeend', markup.join(''));
 
-console.log(galleryItems);
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
