eb5c91c13788e3a82f14a2fcb0926e529806b0a7
RomanMatkovskyi <121770356+RomanMatkovskyi@users.noreply.github.com>
2023-06-04 10:51:20 +0200

    finish HW

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+let formEl = document.querySelector('.feedback-form');
+let inputEl = formEl.querySelector('input');
+let textAreaEl = formEl.querySelector('textarea');
+let btnEl = formEl.querySelector('button');
+
+formEl.addEventListener('input', throttle(inputData, 500));
+function inputData() {
+  let data = { email: inputEl.value, message: textAreaEl.value };
+  let feedbackFormState = JSON.stringify(data);
+  localStorage.setItem('feedbackFormState', feedbackFormState);
+}
+
+window.addEventListener('load', loadData);
+function loadData() {
+  if (localStorage.getItem('feedbackFormState')) {
+    let savedData = JSON.parse(localStorage.getItem('feedbackFormState'));
+    inputEl.value = savedData.email;
+    textAreaEl.value = savedData.message;
+  } else {
+    inputEl.value = '';
+    textAreaEl.value = '';
+  }
+}
+
+formEl.addEventListener('submit', submitData);
+
+function submitData(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedbackFormState')));
+  inputEl.value = '';
+  textAreaEl.value = '';
+}
